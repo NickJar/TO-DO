0b27009aa6372e72f1119c29039b42f1ff008568
Nick <nickjar86@gmail.com>
2023-01-19 17:43:06 -0700

    Create basic to-do page

---
+++ src/index.js
@@ -0,0 +1,103 @@
+import './style.css';
+let header = document.getElementById('header');
+let popupForm = document.getElementById('popupForm');
+let createTask = document.createElement('button');
+createTask.textContent = "Create Task";
+createTask.addEventListener("click", function(){
+    popupForm.style.display = '';
+})
+header.appendChild(createTask);
+
+let closeBTN = document.getElementById('close');
+closeBTN.addEventListener("click", function(){
+    popupForm.style.display = "none";
+    document.getElementById('form').reset();
+})
+
+
+
+let mainContent = document.getElementById('main');
+let submitBTN = document.getElementById('submit');
+submitBTN.addEventListener("click", generateTask);
+
+//task constructor
+function tasks(title, description, date, priority){
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.priority = priority;
+}
+
+function getPriorityColor(priority){
+    if(priority == "Low"){
+        return "low"
+    }
+    else if(priority == "Medium"){
+        return "medium";
+    }
+    else{
+        return "high";
+    }
+}
+
+function generateTask(){
+    popupForm.style.display = "none";
+
+    let priority = radioValue();
+    console.log(priority);
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let date = document.getElementById('date').value;
+
+    const ntask = new tasks(title, description, date, priority)
+
+    let color = getPriorityColor(priority);
+    console.log(color);
+    let task = document.createElement('div');
+    task.classList.add("task");
+    task.classList.add(color);
+
+    let complete = document.createElement('input');
+    complete.type = "checkbox";
+
+    let taskTitle = document.createElement('div')
+    taskTitle.innerHTML = ntask.title;
+
+    let taskDescription = document.createElement('div')
+    taskDescription.innerHTML = ntask.description;
+
+    let taskDate = document.createElement('div')
+    taskDate.innerHTML = ntask.date;
+    console.log(ntask.date);
+
+    mainContent.prepend(task);
+    task.appendChild(complete);
+    task.appendChild(taskTitle);
+    task.appendChild(taskDescription);
+    task.appendChild(taskDate);
+
+    
+
+    document.getElementById('form').reset();
+}
+
+function radioValue(){
+    var radios = document.getElementsByName('priority');
+
+  for (var i = 0, length = radios.length; i < length; i++) {
+  if (radios[i].checked) {
+    // do whatever you want with the checked radio
+    return radios[i].value;
+
+   
+  }
+}
+
+
+
+}
+
+
+
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
